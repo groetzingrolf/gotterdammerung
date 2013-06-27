e77527414ca66977f69d7668b88560454a18e141
Mike Dirolf <mike@dirolf.com>
2013-06-27 14:59:24 -0400

    Screw redis - let's just start w/ no caching.
    This reverts commit 37ac90e10a0d79873fccb6c72b431bd225b8244e.

--- app.py
+++ app.py
@@ -1,22 +1,13 @@
 import os
-
 from flask import Flask
-import redis
 
-
-redis_url = os.getenv("REDISTOGO_URL", "redis://localhost:6379")
-redis = redis.from_url(redis_url)
 app = Flask(__name__)
 
-
-@app.route("/")
+@app.route('/')
 def hello():
-    count = int(redis.get("count") or 0)
-    count += 1
-    redis.set("count", count)
-    return "Hello %d" % count
+    return 'Hello World!'
 
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
